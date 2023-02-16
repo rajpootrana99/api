d16ed8b7726e890c3b665dd83722c70c57ff7ae1
Muhammad Rana <muhammadrana523@gmail.com>
2023-02-16 11:45:55 +0500

    Status change of api

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 trait HttpResponses {
     protected function success($data, $message = null, $code = 200){
         return response()->json([
-            'status' => 'Response was Successful.',
+            'status' => true,
             'message' => $message,
             'data' => $data 
         ], $code);
@@ -13,7 +13,7 @@ trait HttpResponses {
 
     protected function error($data, $message = null, $code){
         return response()->json([
-            'status' => 'Error has occured.',
+            'status' => false,
             'message' => $message,
             'data' => $data 
         ], $code);
