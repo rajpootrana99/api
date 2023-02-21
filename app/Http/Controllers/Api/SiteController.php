fd4b83523bc82d94b35af813d1c8664ed98ecfa8
Muhammad Rana <muhammadrana523@gmail.com>
2023-02-21 11:24:25 +0500

    Changes in Fetch Site api

--- app/Http/Controllers/Api/SiteController.php
+++ app/Http/Controllers/Api/SiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Site;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,7 +13,7 @@ class SiteController extends Controller
 {
     use HttpResponses;
     public function fetchSites(){
-        $sites = Site::where('user_id', Auth::id())->get();
+        $sites = User::with('sites')->find(Auth::id());
         return response()->json($sites);
     }
 }
