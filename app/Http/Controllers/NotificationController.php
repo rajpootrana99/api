28d0550a59efd82e486059c289b70835832559c7
Muhammad Rana <muhammadrana523@gmail.com>
2023-04-17 02:45:00 +0500

    Changes in notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Notification;
+use App\Models\Token;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
@@ -25,56 +26,65 @@ class NotificationController extends Controller
     {
         $response = '';
 
-        $SERVER_API_KEY = 'AAAAH13Wawo:APA91bE61OXDrCbPrhfsXw91djC-QKAfgqVBfFaL3ta9pexkMuTmOTfa_xgryZwN45KrFgM-G_VVN8zpbdAfWrIXEEKClwMY3eImdYGUzsx7hFo_HXUxTlDJ0GhXShOxW9y-D5SB4kFI';
+        $SERVER_API_KEY = 'AAAAFwVn-58:APA91bEkI_A6lAZMm1Jx1rBv8drv8pMRyCnWKzuVKc4LA8W0wzTgXNf0FCevcSkIegRwM2y4T0h68_jMnP66uOjZNY8B2DL5k03-POUfDXJzcYgnHwSFQB6cq2NEZXo1nqZTygd8GJbX';
 
+        $tokens = Token::all();
 
-        $data = [
-            "notification" => [
+        foreach ($tokens as $token) {
+            $token_1 = $token->token;
 
-                "title" => $request->input('title'),
+            $data = [
+                "registration_ids" => [
+                    $token_1
+                ],
 
-                "body" => $request->input('body'),
+                "notification" => [
 
-                "sound" => "default" // required for sound on ios
+                    "title" => $request->input('title'),
 
-            ],
+                    "body" => $request->input('body'),
 
-        ];
+                    "sound" => "default" // required for sound on ios
 
-        $dataString = json_encode($data);
+                ],
 
-        $headers = [
+            ];
 
-            'Authorization: key=' . $SERVER_API_KEY,
+            $dataString = json_encode($data);
 
-            'Content-Type: application/json',
+            $headers = [
 
-        ];
+                'Authorization: key=' . $SERVER_API_KEY,
 
-        $ch = curl_init();
+                'Content-Type: application/json',
 
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+            ];
 
-        curl_setopt($ch, CURLOPT_POST, true);
+            $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POST, true);
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        $response = curl_exec($ch);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        if ($response) {
-            Notification::create([
-                'title' => $request->input('title'),
-                'body' => $request->input('body'),
-            ]);
-            return response()->json(['status' => 1, 'message' => 'Notification Send Successfully']);
-        } else {
-            return response()->json(['status' => 0, 'message' => 'Notification Not Send']);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+
+            $response = curl_exec($ch);
+
+            if ($response) {
+                Notification::create([
+                    'title' => $request->input('title'),
+                    'body' => $request->input('body'),
+                ]);
+                return response()->json(['status' => 1, 'message' => 'Notification Send Successfully']);
+            } else {
+                return response()->json(['status' => 0, 'message' => 'Notification Not Send']);
+            }
         }
     }
 
