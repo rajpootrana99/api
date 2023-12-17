889846dec6d81d6a5980a353c60eb12e84ef6aa7
Muhammad Rana <muhammadrana523@gmail.com>
2023-12-17 16:39:11 +0500

    Fix: Tax code options (gst 10%, N/A)

--- resources/views/purchaseOrder/add.blade.php
+++ resources/views/purchaseOrder/add.blade.php
@@ -214,7 +214,7 @@
         html += '<td><input type="text" style="height: 30px" name="items[' + number + '][qty]" id="qty_' + number + '" onkeyup="calculateCost()" class="form-control qty" /></td>';
         html += '<td><input type="text" style="height: 30px" name="items[' + number + '][order_unit_price]" id="unit_price_' + number + '" onkeyup="calculateCost()" class="form-control price" /></td>';
         html += '<td><input type="text" style="height: 30px" name="items[' + number + '][order_total_amount]" id="amount_' + number + '" readonly class="form-control amount" /></td>';
-        html += '<td><select class="select2 form-control" name="items[' + number + '][tax]" id="tax_' + number + '" style="width: 100%; height:30px;" data-placeholder="Select Tax Code"><option value="10">GST 10%</option></select></td>';
+        html += '<td><select class="select2 form-control tax" name="items[' + number + '][tax]" id="tax_' + number + '" onchange="calculateCost()" style="width: 100%; height:30px;" data-placeholder="Select Tax Code"><option value="10">GST 10%</option><option value="0">N/A</option></select></td>';
         if (number > 1) {
             html += '<td><button style="border: none; background-color: #fff" name="addItems" id="addItems"><i class="fa fa-plus-circle"></i></button></td>';
             html += '<td><button style="border: none; background-color: #fff" name="removeItems" id="removeItems"><i class="fa fa-minus-circle"></i></button></td></tr>';
@@ -369,8 +369,9 @@
         document.querySelectorAll(".item").forEach((item) => {
             const quantity = parseInt(item.querySelector(".qty").value) ? parseInt(item.querySelector(".qty").value) : 0;
             const price = parseFloat(item.querySelector(".price").value) ? parseFloat(item.querySelector(".price").value) : 0;
+            const taxVal = parseFloat(item.querySelector(".tax").value) ? parseFloat(item.querySelector(".tax").value) : 0;
             subtotal += quantity * price
-            const itemTax = ((quantity * price) / 100) * 10;
+            const itemTax = ((quantity * price) / 100) * taxVal;
             tax += itemTax;
             total = subtotal + tax;
             const amount = quantity * price;
